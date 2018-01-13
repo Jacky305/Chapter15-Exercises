ddb90ad6de06ccc56fc1bf01bb4b25ccde0716fe
Jacky <email.com>
2018-01-13 20:58:32 +1100

    Exercise 4

--- Chapter15 Exercises/Program.cs
+++ Chapter15 Exercises/Program.cs
@@ -10,6 +10,20 @@ namespace Chapter15_Exercises
     {
         static void Main(string[] args)
         {
+            string[] words = { "Nog", "Cat", "Mouse", "Chill", "Netflix", "Mirror", "Black", "Carrot", "Apple", "Zlastic" };
+
+            //Split words starting with A-M and N-Z
+            var aToM =
+                from w in words
+                where w[0] <= 'M'
+                select w;
+            var nToZ =
+                from w in words
+                where w[0] > 'M'
+                select w;
+
+            Console.WriteLine("There are {0} words starting with A-M.", aToM.Count());
+            Console.WriteLine("There are {0} words starting with N-Z.", nToZ.Count());
         }
     }
 }
